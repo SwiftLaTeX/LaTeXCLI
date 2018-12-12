5e7611775213779564379cae18e380d7f8bb353c
Elliott Wen <jq.elliott.wen@gmail.com>
2018-12-12 17:53:11 +1300

    before commiting

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-DB_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/LaTeXCLI")
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 TICKING_ACCURARCY = 0.05
 TASK_TIMEOUT = 30
